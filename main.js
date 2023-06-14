312f2614de3254aa6f6c1146322d230ba9563c15
Andres M. Prieto <andresprieto2703@gmail.com>
2023-06-14 03:51:13 -0500

    Basics
    Adding basic electron configuration

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require("electron");
+const path = require('path')
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "src/preload.js"),
+    },
+  });
+
+  window.maximize();
+
+  window.loadFile("index.html");
+
+  return window;
+};
+
+app.whenReady().then(() => {
+  const window = createWindow();
+
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
