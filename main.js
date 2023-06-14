8caa2e764f0881ebb25a09d752c66bb19a9da5cf
Andres M. Prieto <andresprieto2703@gmail.com>
2023-06-14 05:05:14 -0500

    Live reload
    Adding live reload to electron to make this thing easier to be developed

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require("electron");
+require('electron-reload')(__dirname);
 const path = require('path')
+
 
 const createWindow = () => {
   const window = new BrowserWindow({
